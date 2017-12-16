e4366a4080486d92488cd5efa3014b5ee2cd49da
Dylan Aubrey <dylanaubrey@gmail.com>
2017-12-16 12:18:45 +0000

    Updating webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
           presets: [
             ['@babel/preset-env', {
               debug: true,
+              modules: false,
               targets: { browsers: 'last 4 versions' },
               useBuiltIns: 'usage',
             }],
