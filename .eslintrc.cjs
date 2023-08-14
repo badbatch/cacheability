307b5e52d652ac87c33d1f22524f7fffc8c2362b
Dylan Aubrey <dylan.aubrey@bjss.com>
2023-08-14 09:30:48 +0100

    chore: config updates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,12 @@ module.exports = {
   },
   root: true,
   rules: {
-    '@typescript-eslint/no-use-before-define': 0,
     // disabled devDependencies due to https://github.com/import-js/eslint-plugin-import/issues/2168
     'import/no-extraneous-dependencies': [
       2,
       {
         devDependencies: true,
         peerDependencies: false,
-      },
-    ],
-    'unicorn/prevent-abbreviations': [
-      2,
-      {
-        ignore: [/.*args.*/i, /.*dev.*/i, /.*params.*/i, /.*props.*/i],
       },
     ],
   },
